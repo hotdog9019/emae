b941b3e28fbd1943bc8a415618b39417df429aed
hotdog9019 <artemelizbaran9019@gmail.com>
2026-02-25 10:58:20 +0300

    12

--- src/data/branches.js
+++
@@ -1,5 +0,0 @@
-
-module.exports = [
- {id:1,name:'Yomayo Arbat',type:'romantic',address:'Арбат',lat:55.752,lng:37.592},
- {id:2,name:'Yomayo City',type:'business',address:'Москва-Сити',lat:55.749,lng:37.537}
-];
